c15f8804b7eabb1d0d4a6173cf0f051553363ece
Pyravlon <42296528+Pyravlon@users.noreply.github.com>
2026-01-29 09:52:28 +0100

    feat: Implementing the BackJob model with BackState and BackupType enums (#1)
    * feat: Implementing the BackJob class with BackState and BackupType enums
    
    * fix: improve .gitignore and apply fixes based on MR review
    
    ---------
    
    Co-authored-by: Enzo Cadière <contact@ecadiere.fr>

--- Models/BackupJob.cs
+++ Models/BackupJob.cs
@@ -9,7 +9,7 @@ namespace Models
         public string Name { get; set; }
         public List<string> SourcePath { get; set; }
         public string TargetPath { get; set; }
-        public BackupType BackupType { get; set; }
+        public BackupState BackupType { get; set; }
         public BackupState BackupState { get; set; }
         public DateTime LastExecution { get; set; }
         public long TotalFile { get; set; }
