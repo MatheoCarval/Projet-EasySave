18c66a0928574c395869c3eacd4189d5f2b78e6b
Pyravlon <42296528+Pyravlon@users.noreply.github.com>
2026-01-29 09:52:28 +0100

    feat: Implementing the BackJob model with BackState and BackupType enums (#1)
    * feat: Implementing the BackJob class with BackState and BackupType enums
    
    * fix: improve .gitignore and apply fixes based on MR review
    
    ---------
    
    Co-authored-by: Enzo Cadière <contact@ecadiere.fr>

---
+++ Models/BackupJob.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Models.Enums;
+
+namespace Models
+{
+    public class BackupJob
+    {
+        public string Name { get; set; }
+        public List<string> SourcePath { get; set; }
+        public string TargetPath { get; set; }
+        public BackupState BackupType { get; set; }
+        public BackupState BackupState { get; set; }
+        public DateTime LastExecution { get; set; }
+        public long TotalFile { get; set; }
+        public long TotalSize { get; set; }
+        public long RemainingFiles { get; set; }
+        public long RemainingSize { get; set; }
+        public string CurrentSourceFile { get; set; }
+        public string CurrentTargetFile { get; set; }
+        public float Progress { get; set; }
+
+        public BackupJob()
+        {
+            SourcePath = new List<string>();
+        }
+
+        public void UpdateProgress()
+        {
+            if (TotalFile > 0)
+            {
+                Progress = ((TotalSize - RemainingSize) * 100 / TotalSize);
+            }
+        }
+
+        public void SetCurrentFile(string sourceFile, string targetFile)
+        {
+            CurrentSourceFile = sourceFile;
+            CurrentTargetFile = targetFile;
+        }
+
+        public void MarkAsCompleted()
+        {
+            LastExecution = DateTime.Now;
+            RemainingFiles = 0;
+            RemainingSize = 0;
+            Progress = 100;
+            BackupState = BackupState.COMPLETED;
+        }
+
+        public void MarkAsError()
+        {
+            Progress = 0;
+            CurrentSourceFile = null;
+            CurrentTargetFile = null;
+            BackupState = BackupState.ERROR;
+        }
+    }
+}
---
+++ Models/Enums/BackupState.cs
@@ -0,0 +1,10 @@
+namespace Models.Enums
+{
+    public enum BackupSate
+    {
+        ACTIVE,
+        PAUSED,
+        COMPLETED,
+        ERROR
+    }
+}
