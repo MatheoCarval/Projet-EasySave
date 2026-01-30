405d54dbf08e41a8e6ce725ca5d9a0dbfe1de88a
Mathéo Carval <matheocarvalgmail.com>
2026-01-30 11:04:11 +0100

    fix: Correct BackupState enum name and update BackupType property in BackupJob model

--- obj/Debug/net8.0/EasySave.AssemblyInfo.cs
+++ obj/Debug/net8.0/EasySave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c689eb256050e833c569fc52cac61fc9a50876f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ec8008b295394e46ce72606ec86733c77fc454")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
