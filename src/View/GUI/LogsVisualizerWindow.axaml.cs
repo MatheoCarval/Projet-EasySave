3c29eb355f904c582f6cd71ecbb08173bb0bc456
Maéva CHALLIES <156900356+MaevaCha13@users.noreply.github.com>
2026-02-11 15:34:00 +0100

    feat(LogsVisualizer): add structure feature

---
+++ src/View/GUI/LogsVisualizerWindow.axaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using EasySave.ViewModels;
+
+namespace EasySave.View.GUI
+{
+    internal class LogsVisualizerWindow : Window
+    {
+    }
+}
---
+++ src/ViewModels/LogsVisualizerViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.ViewModels
+{
+    /// <summary>
+    ///  To show the backup logs in the LogsView, this class is used to bind the data to the view.
+    /// </summary>
+    internal class LogsVisualizerViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Gets the logs content in JSON format to display in the LogsView. This is a placeholder implementation and should be replaced with actual logic to read logs from a file or other source.
+        /// </summary>
+        /// <returns></returns>
+        public string getLogsJsonContent()
+        {
+
+            return "";
+        }
+
+        private string logText;
+
+        public string LogText
+        {
+            get { return logText; }
+            set
+            {
+                logText = value;
+                OnPropertyChanged(nameof(LogText));
+            }
+
+        }
+
+    }
+
+
+}
